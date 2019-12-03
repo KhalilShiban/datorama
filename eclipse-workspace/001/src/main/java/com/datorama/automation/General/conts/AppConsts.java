753aa6be9d0ad0a50034e77852fc65af3df513c8
khalil <khalil@khalil-ThinkPad-T470>
2019-12-03 21:20:22 +0200

    Add app constants file

---
+++ eclipse-workspace/001/src/main/java/com/datorama/automation/General/conts/AppConsts.java
@@ -0,0 +1,8 @@
+package com.datorama.automation.General.conts;
+
+public class AppConsts {
+    public static final String PTSV2_HOST = "http://ptsv2.com";
+    public static final String POSTMAN_ECHO_HOST = "http://postman-echo.com";
+    public static final int THREADS_NUMBER = 5;
+
+}
